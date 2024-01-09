c52455673f83cda1581cb29c05b3feb8295cc4ec
ScorpionInc <d2dejohn@gmail.com>
2024-01-09 15:33:43 -0500

    Update ll function
    Updates ll function to just be for displaying data gathered by new function get-childitemall uses clone-object function for deep copies.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,43 @@
-#Add a ll function that includes hidden and ads
-function ll(){
- return(Get-ChildItem -Force | ForEach-Object {
-  Get-Item $_.FullName -Force
-  Get-Item $_.FullName -Force -Stream * | Select -ExpandProperty PSPath
- })
+# Modified from source:
+#https://stackoverflow.com/questions/9204829/deep-copying-a-psobject
+#https://stackoverflow.com/a/62559171
+function Clone-Object($InputObject){
+ <#
+  .SYNOPSIS
+  Use the serializer to create an independent copy of an object, useful when using an object as a template
+ #>
+ [System.Management.Automation.PSSerializer]::Deserialize(
+  [System.Management.Automation.PSSerializer]::Serialize(
+   $InputObject
+  )
+ );
 }
+
+# Returns some properties of all files and folders as well as additional data streams(ads) as an Object.
+function Get-ChildItemAll([string]$path='.\'){
+ $entries = @();
+ Get-ChildItem -Path "$($path)" -Force | ForEach-Object {
+  $file_entry = [PSCustomObject]@{
+   Mode=$_.Mode
+   PSIsContainer=$_.PSIsContainer
+   Target=$_.Target
+   CreationTime=$_.CreationTime
+   LastAccessTime=$_.LastAccessTime
+   LastWriteTime=$_.LastWriteTime
+   Length=$_.Length
+   Name=$_.Name
+   Extension=($_.Extension).Replace('.','')
+  };
+  $entries += $file_entry;
+  Get-Item $_.FullName -Force -Stream * | Select-Object PSChildName,Stream,Length | Where-Object -Property Stream -NE ':$DATA' | Select-Object Length,PSChildName | ForEach-Object {
+   $entry = Clone-Object($file_entry);
+   $entry.Length = $_.Length;
+   $entry.Name = $_.PSChildName;
+   $entries += $entry;
+  } | Out-Null;
+ } | Out-Null;
+ return($entries);
+}
+
+# Prints common propertied from Get-ChildItemAll results in a table format.
+function ll([string]$path='.\'){(Get-ChildItemAll "$($path)") | Format-Table -Property Mode,LastWriteTime,Length,Name;}
