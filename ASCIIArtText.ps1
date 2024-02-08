10f8591f864cdb6c67c0e7d52aa8e0638baf8a3e
ScorpionInc <d2dejohn@gmail.com>
2024-02-08 08:29:59 -0500

    Update ASCIIArtText.ps1

--- ASCIIArtText.ps1
+++ ASCIIArtText.ps1
@@ -1,10 +1,13 @@
-﻿function initASCIITextTable(){
+﻿function New-ASCIITextTable(){
 	#TODO
+	# Returns hashtable
 	# Target Size is 6 lines and <= 12 wide.
 	# Uppercase initialized as empty strings.
-	$letters_data = @{};
+	#$letters_data = @{};# Case-Insensitive(Wrong)
+	#$letters_data = [hashtable]::new();
+	$letters_data = New-Object -TypeName hashtable
 	foreach($l in 65..90){
-		$letters_data[ [char]$l ] = "";
+		$letters_data[ $([char]$l) ] = "";
 	}
 	$letters_data["A"] += "     /\`n";
 	$letters_data["A"] += "    /  \`n";
@@ -163,18 +166,121 @@
 	$letters_data["Z"] += "<-----/";
 	# Lowercase Initialized as Uppercase
 	foreach($l in 97..122){
-		$letters_data[ [char]$l ] = $letters_data[ [char]$(($l - 32)) ];
+		#Write-Host "Setting letters_data[$([char]$l)] to: '$($letters_data[ [char]$(($l - 32)) ])'." #Debugging
+		$letters_data[ "$([char]$l)" ] = $letters_data[ "$([char]$(($l - 32)))" ];
 	}
-	$letters_data["w"] += "/\        /\`n";
+	$letters_data["a"]  = "/-----\`n";
+	$letters_data["a"] += "| |-| |`n";
+	$letters_data["a"] += "| |-| \`n";
+	$letters_data["a"] += "\----\/";
+	$letters_data["b"]  = "/-\`n";
+	$letters_data["b"] += "| |`n"
+	$letters_data["b"] += "| \---\`n";
+	$letters_data["b"] += "| /-\ |`n"
+	$letters_data["b"] += "| \-/ |`n";
+	$letters_data["b"] += "\-----/";
+	$letters_data["c"]  = "/---|`n";
+	$letters_data["c"] += "| |-/`n";
+	$letters_data["c"] += "| |-\`n";
+	$letters_data["c"] += "\---|";
+	$letters_data["d"]  = "    /-\`n";
+	$letters_data["d"] += "    | |`n"
+	$letters_data["d"] += "/---/ |`n";
+	$letters_data["d"] += "| /-\ |`n"
+	$letters_data["d"] += "| \-/ |`n";
+	$letters_data["d"] += "\-----/";
+	$letters_data["e"]  = "/----\`n";
+	$letters_data["e"] += "| <> |`n";
+	$letters_data["e"] += "| |--/`n";
+	$letters_data["e"] += "| \--\`n";
+	$letters_data["e"] += "\----/";
+	$letters_data["f"]  = "   /---\`n";
+	$letters_data["f"] += "  / /--/`n";
+	$letters_data["f"] += "/-+ \-\`n";
+	$letters_data["f"] += "\-+ +-/`n";
+	$letters_data["f"] += "  | |`n";
+	$letters_data["f"] += "  +-+";
+	$letters_data["h"]  = "/-\`n";
+	$letters_data["h"] += "| |`n";
+	$letters_data["h"] += "| |--\`n";
+	$letters_data["h"] += "| /-\ \`n";
+	$letters_data["h"] += "| | | |`n";
+	$letters_data["h"] += "+-+ +-+";
+	$letters_data["i"] =  " /-\`n";
+	$letters_data["i"] += " \-/`n";
+	$letters_data["i"] += " /-\`n"
+	$letters_data["i"] += " | |`n";
+	$letters_data["i"] += " | |`n";
+	$letters_data["i"] += "/---\";
+	$letters_data["l"]  = "+-+`n";
+	$letters_data["l"] += "| |`n";
+	$letters_data["l"] += "| |`n";
+	$letters_data["l"] += "| |`n";
+	$letters_data["l"] += "| |`n";
+	$letters_data["l"] += "+-+";
+	$letters_data["m"] =  "  /\  /\`n";
+	$letters_data["m"] += " /  \/  \`n";
+	$letters_data["m"] += "/ /\  /\ \`n";
+	$letters_data["m"] += "\/  \/  \/";
+	$letters_data["n"]  = "|-|--\`n";
+	$letters_data["n"] += "| /-\ \`n";
+	$letters_data["n"] += "| | | |`n";
+	$letters_data["n"] += "+-+ +-+";
+	$letters_data["o"]  = "/-----\`n";
+	$letters_data["o"] += "| |-| |`n";
+	$letters_data["o"] += "| |-| |`n";
+	$letters_data["o"] += "\-----/";
+	$letters_data["p"]  = "/-----\`n";
+	$letters_data["p"] += "| +-+ |`n";
+	$letters_data["p"] += "| +-+ |`n";
+	$letters_data["p"] += "| /--/`n";
+	$letters_data["p"] += "| |`n";
+	$letters_data["p"] += "\-/";
+	$letters_data["q"]  = "/-----\`n";
+	$letters_data["q"] += "| |-| |`n";
+	$letters_data["q"] += "| |-| |`n";
+	$letters_data["q"] += "\--\  |`n";
+	$letters_data["q"] += "    | |`n";
+	$letters_data["q"] += "    \-/";
+	$letters_data["r"]  = "/-v--\`n";
+	$letters_data["r"] += "| +--/`n";
+	$letters_data["r"] += "| |`n";
+	$letters_data["r"] += "+-+";
+	$letters_data["t"]  = "  /-\`n";
+	$letters_data["t"] += "/-+ +-\`n";
+	$letters_data["t"] += "\-+ +-/`n";
+	$letters_data["t"] += "  | |`n";
+	$letters_data["t"] += "  | |`n";
+	$letters_data["t"] += "  +-+";
+	$letters_data["u"]  = "+-+   +-+`n";
+	$letters_data["u"] += "| |   | |`n";
+	$letters_data["u"] += " \ \-/ /`n";
+	$letters_data["u"] += "  \---/";
+	$letters_data["v"]  = "/\    /\`n";
+	$letters_data["v"] += "\ \  / /`n";
+	$letters_data["v"] += " \ \/ /`n";
+	$letters_data["v"] += "  \--/";
+	$letters_data["w"] =  "/\        /\`n";
 	$letters_data["w"] += "\ \  /\  / /`n";
 	$letters_data["w"] += " \ \/  \/ /`n";
 	$letters_data["w"] += "  \--/\--/";
+	$letters_data["x"]  = "/\  /\`n";
+	$letters_data["x"] += "\ \/ /`n";
+	$letters_data["x"] += "/ /\ \`n";
+	$letters_data["x"] += "\/  \/";
+	# Symbol(s)
+	$letters_data[" "] =  "    ";
+	$letters_data["."] =  "/-\`n";
+	$letters_data["."] += "\-/";
+	$letters_data[","] =  "/-\`n";
+	$letters_data[","] +=  "\  |`n";
+	$letters_data[","] += " |/";
 	return($letters_data);
 }
 # Returns the height of a specific character in by line breaks
-function Get-ASCIIArtCharHeight([char]$Character, [array]$ASCIIArtSet = @()){
+function Get-ASCIIArtCharHeight([char]$Character, [hashtable]$ASCIIArtSet = @()){
 	if($ASCIIArtSet.ContainsKey($Character)){
-		$art = $ASCIIArtSet[$Character]
+		$art = $ASCIIArtSet["$($Character)"]
 		$counter = 0
 		ForEach($line in $($art -split "[\r\n]+")){
 			$counter += 1
@@ -184,16 +290,36 @@ function Get-ASCIIArtCharHeight([char]$Character, [array]$ASCIIArtSet = @()){
 	return 0
 }
 # Returns the max and min width of the ascii characters rows.
-function Get-ASCIIArtCharWidth([char]$Character, [array]$ASCIIArtSet = @()){
+function Get-ASCIIArtCharWidth([char]$Character, [hashtable]$ASCIIArtSet = @()){
+	if($ASCIIArtSet.ContainsKey($Character)){
+		$art = $ASCIIArtSet["$($Character)"]
+		$counter = 0
+		ForEach($line in $($art -split "[\r\n]+")){
+			$LineLength = $line.Length
+			if($LineLength -gt $counter){
+				$counter = $LineLength
+			}
+		}
+		return $counter
+	}
+	return 0
 }
 
 # Generates ASCII Art to represent string input value.
-function Generate-ASCIIArtText([String]$Text, [array]$ASCIIArtSet = @()){
+function New-ASCIIArtText([String]$Text, [hashtable]$ASCIIArtSet = @()){
 	$buffer = "";
 	$lineCount = 0;
 	$cWidths = @();
 	foreach($c in $Text.ToCharArray()){
+		if(-not $ASCIIArtSet.ContainsKey("$($c)")){
+			# No ASCII Art Defined for character.
+			continue
+		}
 		$nextArt = $ASCIIArtSet["$($c)"];
+		if([string]::IsNullOrEmpty($nextArt)){
+			# Character's art is defined but is empty.
+			continue;
+		}
 		$nextArtLines = $nextArt.Split("`n");
 		$nextLineCount = $nextArtLines.Count;
 		if($lineCount -lt $nextLineCount){
@@ -232,6 +358,7 @@ function Generate-ASCIIArtText([String]$Text, [array]$ASCIIArtSet = @()){
 	return $buffer;
 }
 
-$ASCIIARTTEXT = (initASCIITextTable);
-echo "$(Generate-ASCIIArtText "HANGMAN" $ASCIIARTTEXT)";
-echo "$(Get-ASCIIArtCharHeight('H' $ASCIIARTTEXT))"
+$ASCIIARTTEXT = (New-ASCIITextTable)
+Write-Host "$(New-ASCIIArtText -Text "Jumped over the," -ASCIIArtSet ($ASCIIARTTEXT))"
+#Write-Host "$(New-ASCIIArtText -Text "HANGMAN" -ASCIIArtSet ($ASCIIARTTEXT))"
+#Write-Host "$(Get-ASCIIArtCharWidth 'H' ($ASCIIARTTEXT))"
